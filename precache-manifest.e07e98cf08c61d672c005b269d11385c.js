7b59b153a532b87b55e487bd272c50b8b2131b2a
Gabriel Ledung <rexgnu@Zanitar.local>
2022-04-14 13:32:33 +0200

    first commit

---
+++ precache-manifest.e07e98cf08c61d672c005b269d11385c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e19e82a5fb56ab3f9a242bb654605e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43782a469ad0a34cf40f",
+    "url": "/static/css/main.44eb430b.chunk.css"
+  },
+  {
+    "revision": "35a025d9a3867afa3fd2",
+    "url": "/static/js/2.f9e9de90.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f9e9de90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43782a469ad0a34cf40f",
+    "url": "/static/js/main.5f6e6a84.chunk.js"
+  },
+  {
+    "revision": "5729119c99fe6d28289d",
+    "url": "/static/js/runtime-main.566bba4b.js"
+  },
+  {
+    "revision": "29d40954bf91681f70787bfaa5e2d65b",
+    "url": "/static/media/settle-logo.29d40954.png"
+  }
+]);
